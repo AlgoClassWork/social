02af19d2635e1b071102948c49b00d3cde01e91c
AlgoClassWork <kyxec.play@gmail.com>
2025-05-06 13:13:50 +0600

    Update urls.py

--- images/urls.py
+++ images/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.ImageListView.as_view(), name='image_list'),
     path('detail/<int:id>/', views.ImageDetailView.as_view(), name='image_detail'),
-    path('create/', views.ImageCreateForm.as_view(), name='image_create'),
+    path('create/', views.ImageCreateView.as_view(), name='image_create'),
 ]
