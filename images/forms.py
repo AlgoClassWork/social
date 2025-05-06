2a7a563e09d7d30ce3d520b498b20a3fdec728c6
AlgoClassWork <kyxec.play@gmail.com>
2025-05-06 12:52:55 +0600

    Update forms.py

---
+++ images/forms.py
@@ -0,0 +1,7 @@
+from django import forms 
+from .models import Image
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image 
+        fields = ['title', 'description', 'image']
